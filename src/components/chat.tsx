d023c00f0f7a522125cef722231c5eb6889131fd
minkyung5x5 <minkyung5x5@gmail.com>
2024-07-12 00:19:52 +0900

    edit chat ui

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -3,11 +3,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { Input, Button, List, Avatar, Card } from 'antd';
 import { sendMessage } from '@/app/api/clovastudio/route';
+import Search from 'antd/es/input/Search';
 
 interface Message {
     role: 'user' | 'assistant';
     content: string;
-  }
+}
 
 const Chat = () => {
     const scrollRef = useRef<HTMLDivElement>(null);
@@ -21,30 +22,30 @@ const Chat = () => {
         console.log(messages)
     }, [messages]);
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        if (!inputValue.trim()) return;
-    
-        const userMessage = inputValue;
-        const newMessages: Message[] = [...messages, { role: 'user', content: inputValue }]
-        setMessages([...newMessages, {role: 'assistant', content: '...'}]);
-        setInputValue('');
-    
+    const fetchAPI = async (newMessages: Message[]): Promise<Message> => {
         try {
-          const response = await sendMessage(newMessages);
-          const botMessage = response.choices[0].message;
-          setMessages((prevMessages) => [
+            const response = await sendMessage(newMessages);
+            return response.choices[0].message;
+        } catch (error) {
+            console.error('Error fetching bot response:', error);
+            return { role: 'assistant', content: 'Error fetching response. Please try again.' };
+        }
+    };
+
+    const handleSubmit = async (value: string) => {
+        if (!inputValue.trim()) return;
+
+        const userMessage = inputValue;
+        const newMessages: Message[] = [...messages, { role: 'user', content: inputValue }];
+        setMessages([...newMessages, { role: 'assistant', content: '...' }]);
+        setInputValue('');
+
+        const botMessage = await fetchAPI(newMessages);
+        setMessages((prevMessages) => [
             ...prevMessages.slice(0, -1),
             botMessage,
-          ]);
-        } catch (error) {
-          console.error('Error fetching bot response:', error);
-          setMessages((prevMessages) => [
-            ...prevMessages.slice(0, -1),
-            { role: 'assistant', content: 'Error fetching response. Please try again.' },
-          ]);
-        }
-      };
+        ]);
+    };
 
     return (
         <div className="relative w-full flex flex-col border-r-2 border-indigo-600">
@@ -54,27 +55,23 @@ const Chat = () => {
 
             <div className="flex-1 overflow-y-auto p-4" ref={scrollRef}>
                 {messages.map((item, index) => (
-                    <Card key={index} size="small" className={`mb-2 w-max ${item.role === 'user' ? 'ml-auto' : 'mr-auto'}`}>
+                    <Card key={index} size="small" className={`mb-2 w-fit ${item.role === 'user' ? 'ml-auto' : 'mr-auto'}`}>
                         <div className={`mr-auto flex space-x-2 ${item.role === 'user' ? 'flex-row-reverse space-x-reverse' : ''}`}>
-                            <Avatar size="small" src={`https://api.dicebear.com/7.x/miniavs/svg?seed=3`} />
-                            <div>{item.content}</div>
+                            <Avatar className="shrink-0" size="small" src={`https://api.dicebear.com/7.x/miniavs/svg?seed=3`} />
+                            <div className='whitespace-pre-wrap	'>{item.content}</div>
                         </div>
                     </Card>
                 ))}
             </div>
 
             <div className="h-max p-4">
-                <div className='flex items-center justify-center space-x-2'>
-                    <Input
-                        placeholder="Type your message..."
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        onPressEnter={handleSubmit}
-                    />
-                    <Button type="primary" onClick={handleSubmit}>
-                        Send
-                    </Button>
-                </div>
+                <Search
+                    placeholder="Type your message..."
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onSearch={(value) => handleSubmit(value)}
+                    enterButton="Search"
+                />
 
             </div>
         </div>
