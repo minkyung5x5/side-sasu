11ce951e6bbf8b6a52eab4e38cab772a9b5abd5a
minkyung5x5 <minkyung5x5@gmail.com>
2024-07-14 17:39:17 +0900

    del console.log

--- src/app/api/clova/route.tsx
+++ src/app/api/clova/route.tsx
@@ -17,7 +17,6 @@ export async function POST(request: Request) {
             headers,
             body: JSON.stringify(req),
         });
-        console.log(headers, response)
         if (!response.ok) {
             throw new Error(`Failed to write data to CLOVA API: ${response.statusText}`);
         }
